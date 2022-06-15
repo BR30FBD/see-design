f1c6b8c38d203f168a9a28eeaabbff9b24942175
HackerNITISH <you@example.com>
2022-06-15 10:25:07 +0530

    15-06-2022

--- src/dashbord/index.jsx
+++ src/dashbord/index.jsx
@@ -3,21 +3,22 @@ import { useLocation } from "react-router-dom";
 // import Searchbar from '../searchbar';
 import heart from "./heart.png";
 import download from "./download.jpg";
-import camera from "./camera.png"
+import camera from "./camera.png";
 import searchicon from "./searchicon.png";
-import upload from "./upload.jpg"
+import upload from "./upload.jpg";
 import "./dash.css";
 const Dashboard = () => {
   // const [imgresult,setImgresult]=useState("")
   const [data, setData] = useState("");
   const [panel, setPanel] = useState(false);
   const [value, setValue] = useState("rose");
-  const [similar,setSimilar]=useState("flex");
+  const [similar, setSimilar] = useState("flex");
   // const [img,setImg]=useState("none")
-  const [altvalue,setAltvalue]=useState("")
-  const [display,setDispaly]=useState("none")
-  const [imgsrc ,setImgsrc]=useState("");
-  const [modal,setModal]=useState("none")
+  const [altvalue, setAltvalue] = useState("");
+  const [display, setDispaly] = useState("none");
+  const [imgsrc, setImgsrc] = useState("");
+  const [modal, setModal] = useState("none");
+  const [subs,setsubs]=useState("none")
   console.log(data.hits, "data");
   const location = useLocation();
   console.log(location, "location");
@@ -32,7 +33,7 @@ const Dashboard = () => {
     dataget();
   }
   const dataget = () => {
-    const value=document.getElementById("searchvalue").value;
+    const value = document.getElementById("searchvalue").value;
 
     fetch(
       "https://pixabay.com/api/?key=12763398-42249d3dd1db2f56d3171f8c0&q=" +
@@ -47,21 +48,21 @@ const Dashboard = () => {
     dataget();
   }, []);
 
-  const handledisc=(e)=>{
-    console.log(e.target.id,"idvaluejkhj");
-    const a=document.getElementById(e.target.id);
-    const b=a.getAttribute("src");
-    console.log(b,"imgsrc");
+  const handledisc = (e) => {
+    console.log(e.target.id, "idvaluejkhj");
+    const a = document.getElementById(e.target.id);
+    const b = a.getAttribute("src");
+    console.log(b, "imgsrc");
     setImgsrc(b);
-    setAltvalue(a.getAttribute("alt"))
-     setDispaly("block")
+    setAltvalue(a.getAttribute("alt"));
+    setDispaly("flex");
 
     document.getElementById("view").scrollIntoView();
-  }
+  };
   return (
-    < >
-      <div className="main-section-dashbord" >
-        <div className="main-nav" style={{background:"#fff"}}>
+    <>
+      <div className="main-section-dashbord" style={{position:"relative"}}>
+        <div className="main-nav" style={{ background: "#fff" }}>
           <label className="drop">
             <input type="checkbox" id="target-drop-example" />
             <span className="control">Cateogry</span>
@@ -83,7 +84,7 @@ const Dashboard = () => {
               className="overlay-close"
             ></label>
           </label>
-          <div className="serach_continer" style={{borderLeft:"none"}}>
+          <div className="serach_continer" style={{ borderLeft: "none" }}>
             <div
               id="selected_item"
               style={{
@@ -94,10 +95,16 @@ const Dashboard = () => {
                 justifyContent: "space-around",
                 fontSize: "14px",
                 alignItems: "center",
-                height:"40px"
+                height: "40px",
               }}
             >
-              <p style={{margin:"0px",cursor:"pointer"}} onClick={()=>setSimilar("none")}>x</p><span className="find_similar1">Find Similar:</span>
+              <p
+                style={{ margin: "0px", cursor: "pointer" }}
+                onClick={() => setSimilar("none")}
+              >
+                x
+              </p>
+              <span className="find_similar1">Find Similar:</span>
             </div>
             <label htmlFor="searachicon">
               <img
@@ -118,7 +125,7 @@ const Dashboard = () => {
               name="ftyfu"
               placeholders="search...."
               id="searchvalue"
-              style={{paddingLeft:"10px"}}
+              style={{ paddingLeft: "10px" }}
             />
             <input type="file" id="file" style={{ display: "none" }} />
             <label htmlFor="file">
@@ -134,7 +141,7 @@ const Dashboard = () => {
             value="search"
             style={{ height: "50px", width: "100px", margin: "10px" }}
           />
-          <label className="drop" style={{border:"none"}}>
+          <label className="drop" style={{ border: "none" }}>
             <input type="checkbox" id="target-drop-example" />
             <span className="control">My Libraries</span>
 
@@ -156,7 +163,7 @@ const Dashboard = () => {
             ></label>
           </label>
         </div>
-        <div className="main-dash">
+        <div className="main-dash" style={{position:"relative"}}>
           <div className="content">
             <div
               className="mini-dash"
@@ -172,6 +179,171 @@ const Dashboard = () => {
               >
                 {location.state.txt}
               </h5>
+      <div style={{width:"100vw",height:"100vh",background:"rgb(20 19 19 / 87%)",position:"fixed",zIndex:"999999999",top:"0px", display: `${display}`,justifyContent:"center",alignItems:"center" }}>
+      <span style={{top:"50px",position:"fixed",right:"50px",fontSize:"40px",color:"#ffff",cursor:"pointer"}} onClick={()=>setDispaly("none")}>X</span>
+      <div
+                  class="card mb-3"
+                 
+                  id="view"
+                  style={{width:"60vw"}}
+                >
+                  <div class="row g-0">
+                    <div class="col-md-6">
+                      <img
+                        src={imgsrc}
+                        class="img-fluid rounded-start"
+                        id="imgdisc"
+                        alt="..."
+                        style={{ height: "100%" }}
+                      />
+                    </div>
+                    <div class="col-md-6">
+                      <div class="card-body">
+                        <h5 class="card-title">{altvalue}</h5>
+                        <p class="card-text text-dark">
+                          This is a wider card with supporting text below as a
+                          natural lead-in to additional content. This content is
+                          a little bit longer.
+                        </p>
+                        <p class="card-text">
+                          <small class="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </p>
+                        {/* <label htmlFor="imgdisc">Download</label> */}
+                        <div
+                          style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            justifyContent: "space-around",
+                            flexWrap:"wrap"
+                          }}
+                        >
+                          <button
+                            style={{
+                              width: "150px",
+                              height: "40px",
+                              border: "none",
+                              margin:"10px"
+                            }}
+                          >
+                            <img
+                              src={heart}
+                              alt=""
+                              style={{
+                                width: "20px",
+                                height: "20px",
+                                marginRight: "10px",
+                              }}
+                            />
+                            save to Library
+                          </button>
+                          <button
+                            style={{
+                              width: "180px",
+                              height: "40px",
+                              border: "none",
+                              margin:"10px"
+                            }}
+                          >
+                            <img
+                              src={download}
+                              alt=""
+                              style={{
+                                width: "20px",
+                                height: "20px",
+                                marginRight: "10px",
+                              }}
+                            />
+                            Download Preview
+                          </button>
+                          <button
+                            style={{
+                              width: "150px",
+                              height: "40px",
+                              border: "none",
+                              margin:"10px"
+                            }}
+                          >
+                            <img
+                              src={upload}
+                              alt=""
+                              style={{
+                                width: "20px",
+                                height: "20px",
+                                marginRight: "10px",
+                              }}
+                            />
+                            Open in App
+                          </button>
+                        </div>
+                        <button
+                          style={{
+                            background: "royalblue",
+                            color: "white",
+                            border: "none",
+                            width: "200px",
+                            height: "40px",
+                            borderRadius: "10px",
+                            margin: "35px",
+                          }}
+                          onClick={()=>setsubs("flex")}
+                        >
+                          Download free with trail
+                        </button>
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+      </div>
+      <div style={{width:"100vw",height:"100vh",background:"rgb(20 19 19 / 87%)",position:"fixed",zIndex:"99999999999",top:"0px", display:`${subs}`,justifyContent:"center",alignItems:"center" }}>
+<div style={{width:"600px",height:"500px",background:"#fff"}}>
+  <div style={{width:"100%",borderBottom:"1px solid"}}>
+    <span style={{float:"right",marginRight:"20px",marginTop:"10px",fontSize:"20px",cursor:"pointer"}} onClick={()=>setsubs("none")}>X</span>
+    <br/>
+    <p style={{textAlign:"center"}}>Confirm your selection</p>
+    
+  
+  </div>
+  <div style={{display:"flex",justifyContent:"space-around"}}>
+    <div style={{width:"100%",height:"400px",display:"flex",flexDirection:"column",alignItems:"center"}}>
+      <p>Monthly Subscription|Learn more</p>
+      <div style={{width:"250px",border:"1px solid royalblue"}}>
+        <div style={{display:"flex",alignItems:"center"}}>
+        <input type="radio"/><span style={{fontSize:"12px",marginLeft:"10px"}}> <b>3 standard assets a month</b></span>
+        </div>
+        <ul>
+          <li>₹2,394.22/month</li>
+          <li>Cancel anytime</li>
+          <li>Rollover up to 36 standard assets with renewal</li>
+        </ul>
+      </div>
+    </div>
+    <div style={{width:"100%",height:"400px"}}>
+    <div style={{width:"100%",height:"400px",display:"flex",flexDirection:"column",alignItems:"center"}}>
+      <p>Annual subscription|Learn more</p>
+      <div style={{width:"250px",border:"1px solid royalblue"}}>
+        <div style={{display:"flex",alignItems:"center"}}>
+        <input type="radio"/><span style={{fontSize:"12px",marginLeft:"10px"}}> <b>10 standard assets a month</b></span>
+        </div>
+        <ul>
+          <li>Get 10 free standard assets during your 30-day free trial</li>
+          <li>Cancel risk-free before your free trial ends</li>
+          <li>₹2,394.22/month on annual plan after free trial ends</li>
+          <li>Rollover up to 120 standard assets with renewal</li>
+        </ul>
+      </div>
+    </div>
+    </div>
+  </div>
+  <div style={{width:"100%",height:"80px",background:"royalblue",display:"flex",justifyContent:"center",alignItems:"center"}}>
+    <button style={{width:"100px",height:"40px",borderRadius:"40px",border:"2px solid white",background:"royalblue",color:"white"}}>Continue</button>
+  </div>
+
+</div>
+
+</div>
               <div className="img-upload" style={{ marginLeft: "32px" }}>
                 <img
                   src={location.state.name}
@@ -193,7 +365,11 @@ const Dashboard = () => {
                     color: "#fff",
                   }}
                 >
-                  <h3 style={{ textAlign: "center",textTransform:"capitalize" }}>Search result</h3>
+                  <h3
+                    style={{ textAlign: "center", textTransform: "capitalize" }}
+                  >
+                    Search result
+                  </h3>
                 </div>
 
                 <div
@@ -218,14 +394,16 @@ const Dashboard = () => {
                       display: "flex",
                       justifyContent: "center",
                       alignItems: "center",
-                      textTransform:"capitalize"
+                      textTransform: "capitalize",
                     }}
                     onClick={() => setPanel(!panel)}
                   >
                     {panel ? "show Panel" : "hide panel"}
                   </p>
                   <div style={{ marginRight: "30px" }}>
-                    <label for="cars" style={{marginRight:"10px"}}>Sort by:</label>
+                    <label for="cars" style={{ marginRight: "10px" }}>
+                      Sort by:
+                    </label>
                     <select
                       name="cars"
                       id="cars"
@@ -240,66 +418,40 @@ const Dashboard = () => {
                 </div>
                 <div className="row row-cols-4 row-cols-md-4 g-4 mt-2 ">
                   {data &&
-                    data.hits.slice(0, 20).map((data,index) => (
+                    data.hits.slice(0, 20).map((data, index) => (
                       <div className="col">
                         <div
                           className="card group-img"
                           style={{ width: "100%", height: "250px" }}
                         >
-                          
                           <img
                             src={data.previewURL}
                             className="card-img-top group-img"
-                            style={{ width: "100%", height: "100%",position:"relative" }}
+                            style={{
+                              width: "100%",
+                              height: "100%",
+                              position: "relative",
+                            }}
                             alt={data.tags}
                             id={index}
-                            onClick={(e)=>handledisc(e)}
+                            onClick={(e) => handledisc(e)}
                           />
-                {/* <div id="icon" style={{width:"100%",height:"100%",background:"white",position:"absolute",display:"flex"}}>
+                          {/* <div id="icon" style={{width:"100%",height:"100%",background:"white",position:"absolute",display:"flex"}}>
                   <img src={heart} alt="" style={{width:"20px",height:"20px"}}/>
                   <img src={download} alt="" style={{width:"20px",height:"20px"}} />
                   <img src={camera} alt="" style={{width:"20px",height:"20px"}} />
                 </div> */}
-
                         </div>
-                      
                       </div>
-                      
                     ))}
                 </div>
-                <div class="card mb-3" style={{display:`${display}`}} id="view">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src={imgsrc} class="img-fluid rounded-start" id="imgdisc" alt="..." style={{height:"400px"}}/>
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">{altvalue}</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-        {/* <label htmlFor="imgdisc">Download</label> */}
-        <div style={{display:"flex",justifyContent:'center',justifyContent:"space-around"}}>
-        <button style={{width:"150px",height:"40px",border:"none"}}>
-          <img src={heart} alt="" style={{width:"20px",height:"20px",marginRight:"10px"}} />
-          save to Library</button><button style={{width:"180px",height:"40px",border:"none"}}>
-          <img src={download} alt="" style={{width:"20px",height:"20px",marginRight:"10px"}} />
-            Download Preview</button><button style={{width:"150px",height:"40px",border:"none"}}>
-              <img src={upload} alt="" style={{width:"20px",height:"20px",marginRight:"10px"}} />
-              Open in App</button>
-
-        </div>
-        <button style={{background:"royalblue",color:"white",border:"none",width:"200px",height:"40px",borderRadius:"10px",margin:"35px"}}>Download free with trail</button>
-      </div>
-    </div>
-  </div>
-</div>
+               
                 <div id="imageView"></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div style={{width:"100vw",height:"100vh",background:"white",position:"fixed",zIndex:"99999",marginTop:"-80px",overflow:"hidden",overflowY:"hidden",display:`${modal}`}}></div>
     </>
   );
 };
